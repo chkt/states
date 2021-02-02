4160621c2cdbb1359d1376713129f29cbf8f4a08
chkt <ck@chkt.one>
2021-02-02 06:30:41 +0100

    Add StateDescriptionMap to exports

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
 export { Context, State, Switch } from './state';
-export { createTransitionMap as create } from './create';
+export { createTransitionMap as create, StateDescriptionMap } from './create';
 export { modify } from './modify';
 export { bindStateToState, bindContextToState, bindContextToContext, isErrorState } from './traverse';
